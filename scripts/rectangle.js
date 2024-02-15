c382db3b637e5151bc02a800869db794660a49a8
md-zulfikarali <azm.phl28@gmail.com>
2024-02-15 08:07:36 -0500

    triangle and rectangle calculated

---
+++ scripts/rectangle.js
@@ -0,0 +1,22 @@
+function rectangularCalculateButton() {
+    // rectangle width input
+    const rectangleWidthInput = document.getElementById('rectangle-width-input');
+    const widthInputText = rectangleWidthInput.value;
+    const width = parseFloat(widthInputText);
+    console.log(width);
+    // rectangle length input
+    const rectangleLengthInput = document.getElementById('rectangle-length-input');
+    const lengthInputText = rectangleLengthInput.value;
+    const length = parseFloat(lengthInputText);
+        console.log(typeof length);
+        // rectangle area calculate
+        const area = width * length;
+        console.log('Rectangle Area:', area); 
+        // rectangle area value
+        const rectangleAreaSpan = document.getElementById('rectangle-area-span');
+        rectangleAreaSpan.innerText = area;
+        console.log(rectangleAreaSpan);
+
+
+
+}
