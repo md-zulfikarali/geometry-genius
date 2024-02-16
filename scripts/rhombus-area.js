9d4eeb725ccf3dccaa8357a985ef085f7caeb5dd
md-zulfikarali <azm.phl28@gmail.com>
2024-02-15 19:25:32 -0500

    rhombus calculation added

---
+++ scripts/rhombus-area.js
@@ -0,0 +1,19 @@
+function getRhombusAreaById(){
+    const rhombusD1 = rhombusInputValueText('rhombus-d1-input');
+    const rhombusD2 = rhombusInputValueText('rhombus-d2-input');
+    const area = 0.5 * rhombusD1 * rhombusD2;
+    setRhombusArea('rhombus-area', area);
+    console.log(area);
+}
+
+function rhombusInputValueText(rhombusElementId){
+    const rhombusInputText = document.getElementById(rhombusElementId);
+    const inputText = rhombusInputText.value;
+    const value = parseFloat(inputText);
+    return value;
+}
+
+function setRhombusArea(elementId, area){
+    const rhombusArea = document.getElementById(elementId);
+    rhombusArea.innerText = area;
+}
